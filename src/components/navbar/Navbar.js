d5f8fb2c1c50aac7ff6fcfc979c37761b5d615cd
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-01-07 00:30:53 +0600

    home page done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = ({ children }) => {
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <div className="w-full navbar bg-base-100 lg:px-20 px-4 fixed top-0 z-50">
-          <div className="flex-1 px-2 mx-2 text-4xl font-bold text-green-600"><span className='text-rose-600'>Clean</span> Bd</div>
+          <div className="flex-1 px-2 mx-2 text-4xl font-bold text-primary"><span className='text-error'>Clean</span> Bd</div>
           <div className="flex-none lg:hidden">
             <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
--- src/pages/Home/Leanding.js
+++ src/pages/Home/Leanding.js
@@ -2,20 +2,37 @@ import React from 'react';
 import bucketgirl from '../../assets/images/bucketgirl.png'
 const Leanding = () => {
   return (
-    <div className="hero h-screen lg:h-[70vh] bg-accent mt-20">
-      <div className="hero-content flex-col lg:flex-row">
-        <div>
-          <h1 className="lg:text-xl text-lg font-bold pb-2">Best Quality</h1>
-          <h1 className="lg:text-5xl text-4xl font-bold">Professional Cleaning Service</h1>
-          <p className="py-4 max-w-xl">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.</p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
+    <>
+      <div className="hero h-screen lg:h-[60vh] bg-accent mt-16">
+        <div className="hero-content flex-col lg:flex-row">
+          <div>
+            <h1 className="lg:text-xl text-lg font-bold pb-2">Best Quality</h1>
+            <h1 className="lg:text-5xl text-4xl font-bold">Professional Cleaning Service</h1>
+            <p className="py-4 max-w-xl">PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
 
-        <div className='h-[70vh] shrink-0 lg:pb-4 pb-20'>
-          <img src={bucketgirl} className="h-full" alt='' />
+          <div className='h-[60vh] shrink-0 lg:pb-4 pb-20'>
+            <img src={bucketgirl} className="h-full" alt='' />
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className='shadow-lg mx-auto p-8 w-5/6 rounded-2xl mt-[-40px] bg-base-200 z-50 relative'>
+        <h1 className='text-2xl mb-3 text-primary'>Get Free Estimate</h1>
+        <div className='grid lg:grid-cols-4 mg:grid-cols-2 grid-cols-1 gap-2'>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+          <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+        </div>
+        <button className='btn btn-primary mt-3'>REQUEST A QOUTE</button>
+      </div>
+    </>
   );
 };
 
