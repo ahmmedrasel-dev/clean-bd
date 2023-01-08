ca7e987ae25b6f2c2124e2d7698130503efae9ef
Ahmmed Rasel <rahmmed.info@gmail.com>
2023-01-08 20:23:26 +0600

    login page design done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,15 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { themeChange } from 'theme-change';
 
 const Navbar = ({ children }) => {
 
-  useEffect(() => {
-    themeChange(false)
-  }, [])
+  const [light, setLight] = useState(false);
 
   return (
-    <div className="drawer drawer-end">
+    <div className="drawer drawer-end" data-theme={light ? 'light' : 'dark'}>
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <div className="w-full navbar bg-base-100 lg:px-20 px-4 fixed top-0 z-50">
@@ -32,14 +29,14 @@ const Navbar = ({ children }) => {
               <li className='dropdown dropdown-hover dropdown-end'>
                 <label tabIndex={0} className="btn btn-primary btn-outline rounded-lg">Book Now</label>
                 <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                  <li><NavLink to='/contact' className='rounded-lg'>Contact Us</NavLink></li>
-                  <li><NavLink to='/login' className='rounded-lg'>Login</NavLink></li>
+                  <li><NavLink to='/contact' className='rounded-lg'>Pre-Book</NavLink></li>
+                  <li><NavLink to='/login' className='rounded-lg'>Quick Book</NavLink></li>
                 </ul>
               </li>
 
               <label className="swap swap-rotate">
 
-                <input type="checkbox" data-toggle-theme="dark,light" />
+                <input type="checkbox" onClick={() => setLight(!light)} />
 
                 <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
 
@@ -60,6 +57,16 @@ const Navbar = ({ children }) => {
           <li><NavLink to='/about' className='rounded-lg'>About</NavLink></li>
           <li><NavLink to='/contact' className='rounded-lg'>Contact Us</NavLink></li>
           <li><NavLink to='/login' className='rounded-lg'>Login</NavLink></li>
+
+          <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+            <div className="collapse-title text-xl font-medium">
+              Book Now
+            </div>
+            <div className="collapse-content">
+              <li><NavLink to='/login' className='rounded-lg'>Pre-Book</NavLink></li>
+              <li><NavLink to='/login' className='rounded-lg'>Quick Book</NavLink></li>
+            </div>
+          </div>
         </ul>
 
       </div>
